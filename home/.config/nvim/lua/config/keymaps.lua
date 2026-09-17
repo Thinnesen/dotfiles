0e466ed27c257159f2eaebd4e1370658a1b93d1f
Thinnesen <92097991+Thinnesen@users.noreply.github.com>
2026-09-17 19:00:11 +0200

    added herdr-like binds to vim

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- herdr/tmux muscle memory: prefix+" splits below, prefix+% splits beside
+-- (herdr uses ctrl+space as prefix; in vim it is just <leader>)
+vim.keymap.set("n", '<leader>"', "<C-W>s", { desc = "Split Window Below (herdr)", remap = true })
+vim.keymap.set("n", "<leader>%", "<C-W>v", { desc = "Split Window Right (herdr)", remap = true })
